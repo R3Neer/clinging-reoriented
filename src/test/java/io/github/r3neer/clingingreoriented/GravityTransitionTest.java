928fcc038a15e64444780fdc3036640a82eb36b0
Samuel <64842010+R3Neer@users.noreply.github.com>
2026-09-12 15:06:34 +0200

    test: prove interrupted snap compensation is frame-independent

--- src/test/java/io/github/r3neer/clingingreoriented/GravityTransitionTest.java
+++ src/test/java/io/github/r3neer/clingingreoriented/GravityTransitionTest.java
@@ -3,7 +3,6 @@ package io.github.r3neer.clingingreoriented;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.moigferdsrte.gravitychanger.util.RotationUtil;
-import java.util.stream.Stream;
 import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
@@ -46,6 +45,22 @@ final class GravityTransitionTest {
                 Quaternionf startFrame = GravityTransition.compensatedVisualStart(oldFrame, plan.yawDelta());
                 assertVecEquals(transform(oldFrame, oldLocal), transform(startFrame, newLocal), EPS,
                     "composite start " + from + " -> " + to);
+            }
+        }
+    }
+
+    @Test
+    void interruptionCompensationIsContinuousForArbitraryDisplayedFrame() {
+        Quaternionf displayed = new Quaternionf().rotateXYZ(0.31F, -0.77F, 0.43F).normalize();
+        for (float yaw : new float[]{-140.0F, -25.0F, 63.0F, 179.0F}) {
+            for (float pitch : new float[]{-50.0F, 0.0F, 41.0F}) {
+                for (float delta : new float[]{-180.0F, -90.0F, 37.0F, 90.0F, 180.0F}) {
+                    Vec3 oldLocal = RotationUtil.rotToVec(yaw, pitch);
+                    Vec3 newLocal = RotationUtil.rotToVec(Mth.wrapDegrees(yaw + delta), pitch);
+                    Quaternionf compensated = GravityTransition.compensatedVisualStart(displayed, delta);
+                    assertVecEquals(transform(displayed, oldLocal), transform(compensated, newLocal), EPS,
+                        "interrupted frame continuity yaw=" + yaw + " pitch=" + pitch + " delta=" + delta);
+                }
             }
         }
     }
