41944439bb2cb4f62b72f97d93db92a9c1e9251a
Samuel <64842010+R3Neer@users.noreply.github.com>
2026-09-14 19:54:56 +0200

    test: keep Shulker duplication creation probe nonfatal

--- src/gametest/java/io/github/r3neer/clingingreoriented/testmixin/ShulkerDuplicationProbeMixin.java
+++ src/gametest/java/io/github/r3neer/clingingreoriented/testmixin/ShulkerDuplicationProbeMixin.java
@@ -1,10 +1,6 @@
 package io.github.r3neer.clingingreoriented.testmixin;
 
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.EntitySpawnReason;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Shulker;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,14 +16,5 @@ public abstract class ShulkerDuplicationProbeMixin {
     private void clinging$beforeOffspringCreate(CallbackInfo ci){
         Shulker self=(Shulker)(Object)this;
         System.out.println("[SC-DUP-PROBE] before-create uuid="+self.getUUID()+" pos="+self.position()+" health="+self.getHealth()+" max="+self.getMaxHealth());
-    }
-
-    @Inject(
-        method="hitByShulkerBullet",
-        at=@At(value="INVOKE",target="Lnet/minecraft/server/level/ServerLevel;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z")
-    )
-    private void clinging$beforeOffspringAdd(CallbackInfo ci){
-        Shulker self=(Shulker)(Object)this;
-        System.out.println("[SC-DUP-PROBE] before-add uuid="+self.getUUID()+" pos="+self.position()+" health="+self.getHealth()+" max="+self.getMaxHealth());
     }
 }
